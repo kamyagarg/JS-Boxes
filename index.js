eddaa31e782f6f51a7ee82973756a730e3485d9a
Kamya Garg <30886166+kamyagarg@users.noreply.github.com>
2020-12-16 21:29:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+
+
+// alert("hello");
+
+var a = document.getElementById("cost").addEventListener("click", fun1);
+
+function fun1(){
+    // document.getElementById("cost").style.backgroundColor = "lightblue";
+    // document.getElementById("cost").style.borderColor = "red";
+    document.getElementsByClassName("vertical1")[0].style.flexGrow = "4";
+    document.getElementById("cost").style.flexGrow="4";
+
+}
+
+
+
