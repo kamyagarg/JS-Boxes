d7e10ad7d1fc07d1c7441e59d11e90d6f81af03a
Kamya Garg <30886166+kamyagarg@users.noreply.github.com>
2020-12-16 22:34:54 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -4,15 +4,27 @@
 
 // alert("hello");
 
-var a = document.getElementById("cost").addEventListener("click", fun1);
+// var a = document.getElementById("cost").addEventListener("click", fun1);
 
-function fun1(){
-    // document.getElementById("cost").style.backgroundColor = "lightblue";
-    // document.getElementById("cost").style.borderColor = "red";
-    document.getElementsByClassName("vertical1")[0].style.flexGrow = "4";
-    document.getElementById("cost").style.flexGrow="4";
+// function fun1(){
+//     // document.getElementById("cost").style.backgroundColor = "lightblue";
+//     // document.getElementById("cost").style.borderColor = "red";
+//     document.getElementsByClassName("vertical1")[0].style.flexGrow = "4";
+//     document.getElementById("cost").style.flexGrow="4";
 
+// }
+
+function expandBox(e){
+    var selectedEl = document.querySelector(".selected");
+    if(selectedEl){
+        selectedEl.classList.remove("selected");        
+        selectedEl.parentNode.classList.remove("selected2");
+    }
+    console.log(e.className);
+    e.classList.add("selected");
+    let node = e.parentNode;
+    node.classList.add("selected2"); 
+   
 }
-
 
 
